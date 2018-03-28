c3945a9be6f4277450b38935c03566a7e409287f
blaze6950 <d.progect21@gmail.com>
2018-03-28 16:21:19 +0300

    Empty project

---
+++ app/src/main/java/zna/online/compass/Authorization.java
@@ -0,0 +1,13 @@
+package zna.online.compass;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Authorization extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authorization);
+    }
+}
